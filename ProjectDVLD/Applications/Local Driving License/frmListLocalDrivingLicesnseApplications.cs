e5dc94167093304c336f73757422cac0c416a7d7
MohamedRoshdy404 <asdenn29@gmail.com>
2026-01-13 21:02:51 +0200

    first commit

--- ProjectDVLD/Applications/Local Driving License/frmListLocalDrivingLicesnseApplications.cs
+++ ProjectDVLD/Applications/Local Driving License/frmListLocalDrivingLicesnseApplications.cs
@@ -44,7 +44,7 @@ namespace ProjectDVLD.Applications.Local_Driving_License
             if (dgvLocalDrivingLicenseApplications.Rows.Count > 0)
             {
                 dgvLocalDrivingLicenseApplications.Columns[0].HeaderText = "L.D.L.AppID";
-                dgvLocalDrivingLicenseApplications.Columns[0].Width = 120;
+                dgvLocalDrivingLicenseApplications.Columns[0].Width = 100;
 
                 dgvLocalDrivingLicenseApplications.Columns[1].HeaderText = "Driving Class";
                 dgvLocalDrivingLicenseApplications.Columns[1].Width = 300;
@@ -53,13 +53,13 @@ namespace ProjectDVLD.Applications.Local_Driving_License
                 dgvLocalDrivingLicenseApplications.Columns[2].Width = 150;
 
                 dgvLocalDrivingLicenseApplications.Columns[3].HeaderText = "Full Name";
-                dgvLocalDrivingLicenseApplications.Columns[3].Width = 350;
+                dgvLocalDrivingLicenseApplications.Columns[3].Width = 280;
 
                 dgvLocalDrivingLicenseApplications.Columns[4].HeaderText = "Application Date";
                 dgvLocalDrivingLicenseApplications.Columns[4].Width = 170;
 
                 dgvLocalDrivingLicenseApplications.Columns[5].HeaderText = "Passed Tests";
-                dgvLocalDrivingLicenseApplications.Columns[5].Width = 150;
+                dgvLocalDrivingLicenseApplications.Columns[5].Width = 120;
             }
 
             cbFilterBy.SelectedIndex = 0;
@@ -285,17 +285,18 @@ namespace ProjectDVLD.Applications.Local_Driving_License
                         break;
 
                     case 1:
-                    case 2:
                         scheduleVisionTestToolStripMenuItem.Enabled = false;
                         scheduleWrittenTestToolStripMenuItem.Enabled = true;
                         scheduleStreetTestToolStripMenuItem.Enabled = false;
                         break;
 
-                    case 3:
+                    case 2:
                         scheduleVisionTestToolStripMenuItem.Enabled = false;
                         scheduleWrittenTestToolStripMenuItem.Enabled = false;
-                        scheduleStreetTestToolStripMenuItem.Enabled = false;
+                        scheduleStreetTestToolStripMenuItem.Enabled = true;
                         break;
+
+
 
                     default:
                         ScheduleTestsMenue.Enabled = false;
